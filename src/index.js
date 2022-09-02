1c3d7b2ce4cf1b2c2e9f9b8d1665c76074a54c6e
TJ-R <57290416+TJ-R@users.noreply.github.com>
2022-09-01 20:38:58 -0400

    Finished Tutorial

--- src/index.js
+++ src/index.js
@@ -1,17 +1,168 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Functional component to render each square
+function Square(props) {
+    return (
+        <button 
+            className="square"
+            onClick={props.onClick}
+        >
+            {props.value}
+        </button>
+    );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Functional component to render the board with 9 squares  
+class Board extends React.Component { 
+    // Calls functional component passing values based on index of the
+    // square on the board
+    renderSquare(i) {
+        return <Square 
+                    value={this.props.squares[i]}
+                    onClick={() => this.props.onClick(i)}
+                />;
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        );
+    }
+}
+
+// Class component that keeps track of game state
+class Game extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            history: [{
+                squares: Array(9).fill(null),
+            }],
+            stepNumber: 0,
+            xIsNext: true,
+        };
+    }
+
+    /* Function to handle on clicking of square 
+    *  makes copy of history
+    */
+    handleClick(i) {
+        const history = this.state.history.slice(0, this.state.stepNumber + 1); //get entire history from start to current stepNumber
+        const current = history[history.length - 1]; //grab last history
+        const squares = current.squares.slice(); //copy current so that new move can be made
+        
+        // Check if winner has been decided or if square is occupied
+        // Return early
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+
+        //Change square value to respective player then
+        //Add new board to history, set new length, change players turn
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            history: history.concat([{
+                squares: squares,
+            }]),
+            stepNumber: history.length,
+            xIsNext: !this.state.xIsNext,
+        });
+    } 
+
+    /* Function to travel back
+    *   set stepNumber to where we are traveling since the current board
+    *   is rendered based on current step number
+    *   modify xIsNext so the appropriate player turn is displayed
+    */
+    jumpTo(step) {
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step % 2) === 0,
+        });
+    }
+
+    render() {
+        const history = this.state.history;
+        const current = history[this.state.stepNumber];
+        const winner = calculateWinner(current.squares);
+
+        // map history as a set of moves that can
+        // be jumped to on button press
+        const moves = history.map((step, move) => {
+            const desc = move ?
+                'Go to move #' + move :
+                'Go to game start';
+            return (
+                <li key={move}>
+                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
+                </li>
+            );
+        });
+
+        let status;
+        if (winner) {
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
+
+        return (
+        <div className="game">
+            <div className="game-board">
+                <Board
+                    squares={current.squares}
+                    onClick={(i) => this.handleClick(i)} 
+                />
+            </div>
+            <div className="game-info">
+                <div>{status}</div>
+                <ol>{moves}</ol>
+            </div>
+        </div>
+        );
+    }
+}
+
+// Function to see if any of the possible conditions to win have been met
+function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
+// ========================================
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Game />);
